f75838a9572550a48b949ea8f3b0303b02c8d8bf
r1z4x <rizax@tuta.io>
2024-08-24 12:51:14 +0300

    bugs fixed

--- src/Cvss.php
+++ src/Cvss.php
@@ -25,10 +25,10 @@ class CVSS
     private const V3_VALIDATION_REGEX = '/^CVSS:(3.1|3.0)\/AV:[NALP]\/AC:[LH]\/PR:[NLH]\/UI:[NR]\/S:[UC]\/C:[NLH]\/I:[NLH]\/A:[NLH]/';
     private const V2_VALIDATION_REGEX = '/AV:[LAN]\/AC:[HML]\/Au:[MSN]\/C:[NCP]\/I:[NCP]\/A:[NCP]/';
 
-    public static function generateScores(string $vector): CvssResults
+    public static function generateScores(string $vector): CVSSResults
     {
         if (!self::validateVector($vector)) {
-            throw CvssException::invalidVector();
+            throw CVSSException::invalidVector();
         }
 
         $vectorVersion = self::getVectorVersion($vector);
@@ -43,32 +43,32 @@ class CVSS
         return $cvssObject->getResults();
     }
 
-    private static function parseVector(string $vector, string $version): CvssObject
+    private static function parseVector(string $vector, string $version): CVSSObject
     {
         return match ($version) {
-            Cvss23Object::VERSION_2 => Cvss2Parser::parseVector($vector),
-            Cvss23Object::VERSION_30, Cvss23Object::VERSION_31 => Cvss31Parser::parseVector($vector),
-            Cvss23Object::VERSION_40 => (new Cvss40Parser())->parseVector($vector),
+            CVSS23Object::VERSION_2 => CVSS2Parser::parseVector($vector),
+            CVSS23Object::VERSION_30, CVSS23Object::VERSION_31 => CVSS31Parser::parseVector($vector),
+            CVSS23Object::VERSION_40 => (new CVSS40Parser())->parseVector($vector),
         };
     }
 
-    private static function buildCalculator(string $version): CvssCalculator
+    private static function buildCalculator(string $version): CVSSCalculator
     {
         return match ($version) {
-            Cvss23Object::VERSION_2 => new Cvss2Calculator(),
-            Cvss23Object::VERSION_30 => new Cvss30Calculator(),
-            Cvss23Object::VERSION_31 => new Cvss31Calculator(),
-            Cvss23Object::VERSION_40 => new Cvss40Calculator(),
-            default => throw CvssException::invalidVector(),
+            CVSS23Object::VERSION_2 => new CVSS2Calculator(),
+            CVSS23Object::VERSION_30 => new CVSS30Calculator(),
+            CVSS23Object::VERSION_31 => new CVSS31Calculator(),
+            CVSS23Object::VERSION_40 => new CVSS40Calculator(),
+            default => throw CVSSException::invalidVector(),
         };
     }
 
     private static function validateVector(string $vector): bool
     {
-        return self::validCvssFourVector($vector) || self::validCvssThreeVector($vector) || self::validCvssTwoVector($vector);
+        return self::validCVSSFourVector($vector) || self::validCVSSThreeVector($vector) || self::validCVSSTwoVector($vector);
     }
 
-    private static function validCvssFourVector(string $vector): bool
+    private static function validCVSSFourVector(string $vector): bool
     {
         if (!(bool)preg_match(self::V4_VALIDATION_REGEX, $vector, $matches)) {
             return false;
@@ -85,20 +85,20 @@ class CVSS
         return true;
     }
 
-    private static function validCvssThreeVector(string $vector): bool
+    private static function validCVSSThreeVector(string $vector): bool
     {
         return (bool)preg_match(self::V3_VALIDATION_REGEX, $vector);
     }
 
-    private static function validCvssTwoVector(string $vector): bool
+    private static function validCVSSTwoVector(string $vector): bool
     {
         return (bool)preg_match(self::V2_VALIDATION_REGEX, $vector);
     }
 
     private static function getVectorVersion(string $vector): string
     {
-        if (self::validCvssTwoVector($vector)) {
-            return Cvss23Object::VERSION_2;
+        if (self::validCVSSTwoVector($vector)) {
+            return CVSS23Object::VERSION_2;
         }
 
         return explode(':', explode('/', $vector)[0])[1];
